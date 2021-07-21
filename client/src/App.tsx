d1d6cad7777bd765289f444dabd6a79267b9284d
Robbie Davenport <davenpra@mail.uc.edu>
2021-07-20 22:55:01 -0400

    initial website changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useCallback } from "react";
 
 function App() {
+  const handleButtonClick = useCallback(() => {
+    alert("aaaaaaaaaaaaaaaa");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span style={{ fontSize: "40px" }}>Not The Onion </span>
+        <img
+          src={
+            "https://i5.walmartimages.com/asr/9ef999d2-a1d2-4ab7-bde3-70995f9aa155_1.3979fa6701d2b8528eb2d8e9aa6ead68.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+
+        <p></p>
+        <button onClick={handleButtonClick}>Generate New</button>
       </header>
     </div>
   );
