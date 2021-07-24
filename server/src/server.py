2949badb54ff64ad59b591eb0be899dfb3824c9b
Nick Lay <layns@mail.uc.edu>
2021-07-23 22:50:00 -0400

    add server container

---
+++ server/src/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'HELLO WORLD'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
