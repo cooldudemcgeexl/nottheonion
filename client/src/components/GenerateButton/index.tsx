36fb4e49af677eab926f0b67f3c4af8e533578b9
Robbie Davenport <davenpra@mail.uc.edu>
2021-08-03 21:11:21 -0400

    Theming and Dark Mode

--- client/src/components/GenerateButton/index.tsx
+++ client/src/components/GenerateButton/index.tsx
@@ -3,12 +3,19 @@ import { FC } from "react";
 import "./styles.scss";
 
 type GenButtonProps = {
+  isLightMode?: boolean;
   handleButtonClick?: () => void;
 };
 
-const GenerateButton: FC<GenButtonProps> = ({ handleButtonClick }) => {
+const GenerateButton: FC<GenButtonProps> = ({
+  isLightMode,
+  handleButtonClick,
+}) => {
   return (
-    <button className="Gen-Button-Style" onClick={handleButtonClick}>
+    <button
+      className={isLightMode ? "Gen-Button-Style" : "Gen-Button-Style-Dark"}
+      onClick={handleButtonClick}
+    >
       Generate New Article
     </button>
   );
